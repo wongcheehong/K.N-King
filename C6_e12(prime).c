a629c8d2863371a856a50e0bcff1fd4ba992d5aa
wongcheehong <wongcheehong2001@gmail.com>
2019-07-06 15:26:57 +0800

    Add files via upload

---
+++ C6_e12(prime).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int n, d ;
+
+    printf("This program check whether a number is a prime.\nCreated by Handsome Hong.");
+    printf("\nEnter a number (Enter 0 to exit): ");
+    scanf("%d", &n);
+
+    while (n != 0){
+        for (d=2; d*d <= n ; d++){
+            if(n%d == 0)
+                break;
+        }
+        if (d*d <= n)
+            printf("%d is divisible by %d\n", n, d);
+            else
+            printf("%d is a prime number\n", n);
+    }
+
+
+}
