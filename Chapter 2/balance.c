1bd372c27996a1ce41004be7386f3826314868ec
wongcheehong <wongcheehong2001@gmail.com>
2019-07-07 14:56:27 +0800

    Update balance.c

--- Chapter 2/balance.c
+++ Chapter 2/balance.c
@@ -2,25 +2,24 @@
 
 int main(void)
 {
-    float loan, interest_rate, monthly_payment, monthly_increase_rate, first, second, third;
+    float loan, interest, monthlyPayment;
 
-    printf("Enter amount of loan: ");
+    printf("\nEnter the amount of loan: ");
     scanf("%f", &loan);
-
-    printf("Enter yearly interest rate:");
-    scanf("%f", &interest_rate);
-
+    printf("Enter interest rate: ");
+    scanf("%f", &interest);
     printf("Enter monthly payment: ");
-    scanf("%f", &monthly_payment);
+    scanf("%f", &monthlyPayment);
 
-    monthly_increase_rate=(1+ interest_rate/1200);
-    first=(loan - monthly_payment)* monthly_increase_rate;
-    second=(first - monthly_payment) * monthly_increase_rate ;
-    third=(second - monthly_payment)* monthly_increase_rate ;
+    /* interest is yearly, so this converts it to a monthly rate */
+    interest = (interest * 0.01f) / 12;
 
-    printf("Balance remaining after first payment: $%.2f\n", first);
-    printf("Balance remaining after second payment: $%.2f\n", second);
-    printf("Balance remaining after third payment: $%.2f\n", third);
+    loan = (loan - monthlyPayment) + (loan * interest);
+    printf("\nBalance remaining after first payment:  $%.2f", loan);
+    loan = (loan - monthlyPayment) + (loan * interest);
+    printf("\nBalance remaining after second payment: $%.2f", loan);
+    loan = (loan - monthlyPayment) + (loan * interest);
+    printf("\nBalance remaining after third payment:  $%.2f\n", loan);
 
-    return(0);
+    return 0;
 }
